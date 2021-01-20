6a410c5f5fec467cc17050737d34926c8527ff02
1761640883 <1761640883@qq.com>
2021-01-20 21:50:52 +0800

    modify

--- src/views/movie/detail.vue
+++ src/views/movie/detail.vue
@@ -6,7 +6,7 @@
 		<loading v-if="isLoading" />
 		<div v-else id="content" class="contentDetail">
 			<div class="detail_list">
-				<div class="detail_list_bg"></div>
+				<div class="detail_list_bg" :style="{'background': `url('${datalist.poster}')`}"></div>
 				<div class="detail_list_filter"></div>
 				<div class="detail_list_content">
 					<div class="detail_list_img">
@@ -96,7 +96,7 @@ export default {
 	}
 	#content.contentDetail{ display: block; margin-bottom:0;}
 	#content .detail_list{ height:200px; width:100%; position: relative; overflow: hidden;}
-	.detail_list .detail_list_bg{ width:100%; height:100%; background: url(/images/movie_1.jpg) 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
+	.detail_list .detail_list_bg{ width:100%; height:100%; background: url(/images/movie_1.jpg) 0 40%; background-size:cover; position: absolute; left: 0; top: 0;filter: blur(20px);}
 	.detail_list .detail_list_filter{ width:100%; height:100%; position: absolute;background-color: #40454d;opacity: .55; position: absolute; left: 0; top: 0; z-index: 1;}
 	.detail_list .detail_list_content{ display: flex; width:100%; height:100%; position: absolute; left: 0; top: 0; z-index: 2;}
 	.detail_list .detail_list_img{ width:108px; height: 150px; border: solid 1px #f0f2f3; margin:20px;}
@@ -108,6 +108,7 @@ export default {
 	#content .detail_intro{ padding: 10px;}
 	#content .detail_player{ margin:20px;}
 	.detail_player .swiper-slide{ width:70px; margin-right: 20px; text-align: center; font-size: 14px;}
+	.detail_player .swiper-slide div{width: 100%;height: 80px;overflow: hidden;}
 	.detail_player .swiper-slide img{ width:100%; margin-bottom: 5px;}
 	.detail_player .swiper-slide p:nth-of-type(2){ color:#999;}
 </style>
